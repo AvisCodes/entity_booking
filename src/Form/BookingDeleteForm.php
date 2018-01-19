61678861d98f1aba1e24e6c8a65bfc141f5dafe6
dima <dima@avis.codes>
2018-01-19 16:50:39 +0200

    init

---
+++ src/Form/BookingDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\entity_booking\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Booking entities.
+ *
+ * @ingroup entity_booking
+ */
+class BookingDeleteForm extends ContentEntityDeleteForm {
+
+
+}
